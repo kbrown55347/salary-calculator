5a399fe81a68ab5eb7ace0cdd11ba4aac52a3a74
Kelsey Brown <kbrown55347@gmail.com>
2021-11-06 18:26:44 -0500

    added clear input values functionality, created function & used jQuery to add employee input information to DOM

--- script.js
+++ script.js
@@ -10,14 +10,12 @@ let employeeList = [];
 // Create handleSubmitEmployeeClick function that collects info from 
 // input fields and adds to employeeList array
 function handleSubmitEmployeeClick() {
-
     // Set variables equal to .val() from each input field
     let newFirstName = $('#first-name-input').val();
     let newLastName = $('#last-name-input').val();
     let newIdNumber = $('#id-input').val();
     let newJobTitle = $('#job-title-input').val();
     let newAnnualSalary = $('#salary-input').val();
-
     // add employee info as object
     let newEmployee = {
         firstName: newFirstName,
@@ -26,8 +24,37 @@ function handleSubmitEmployeeClick() {
         jobTitle: newJobTitle,
         annualSalary: Number(newAnnualSalary),
     }
-
-    //push to employeeList array
+    // push to employeeList array
     employeeList.push(newEmployee);
-    console.log(newEmployee);
+
+    // clear input fields
+    $('#first-name-input').val('');
+    $('#last-name-input').val('');
+    $('#id-input').val('');
+    $('#job-title-input').val('');
+    $('#salary-input').val('');
+
+    // call appendEmployeeInfo 
+    appendEmployeeInfo(employeeList);
+}
+
+// Create function to empty then append employeeList info onto DOM in table
+function appendEmployeeInfo(employees) {
+    $('#employee-table-body').empty();
+    // loop through each employee in employeeList
+    for (let employee of employees) {
+    // add info for each employee as row in table
+        let newTableRow = `
+            <tr>
+                <td>${employee.firstName}</td>
+                <td>${employee.lastName}</td>
+                <td>${employee.idNumber}</td>
+                <td>${employee.jobTitle}</td>
+                <td>${employee.annualSalary}</td>
+                <td><button id='delete-button'>Delete</button></td>
+            </tr>
+        `;
+        $('#employee-table-body').append(newTableRow);
+    }
+    console.log(employeeList);
 }
